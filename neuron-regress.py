e08c76d3f4808be7f3fdca6616e272ec7bedfea0
Dimitrios Damopoulos <dimdamop@hotmail.com>
2019-01-27 13:49:49 +0100

    Rename main.py to neuron-regress.py. Add argument parse functionality. Make it Python 2.7 compliant

--- neuron-regress.py
+++ neuron-regress.py
@@ -45,28 +45,29 @@ class LinearActivationNeuron(object):
                   'learning rate and/or by having the \'normalize\' option disabled.\n')
 
 
-def predict_with_linear_neuron(X_train, y_train, X_test, lrate, epochs, 
+def predict_with_linear_neuron(X_train, y_train, X_valid, lrate, epochs, 
                         on_epoch_end_callback=None):
 
     dims = X_train.shape[1]
     model = LinearActivationNeuron(dims, param_initializer=np.zeros) 
     model.fit(X_train, y_train, lrate, epochs, on_epoch_end_callback)
-    predictions = model.predict(X_test)
+    predictions = model.predict(X_valid)
     parameters = model.theta
     
     return predictions, parameters
 
 
-def predict_with_linear_regression(X_train, y_train, X_test):
+def predict_with_linear_regression(X_train, y_train, X_valid):
 
     from sklearn.linear_model import LinearRegression as LR
 
     model = LR()
     model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
+    predictions = model.predict(X_valid)
     parameters = np.insert(model.coef_, 0, model.intercept_)
     
     return predictions, parameters
+
 
 def parse_csv(fn, sep=',', name_delim='"'):
 
@@ -96,6 +97,7 @@ class RmseRecorder(object):
         self.y = y
         self.losses = np.full(epochs, np.nan)
 
+    @staticmethod
     def rmse(a, b):
         e = a - b
         mse = norm(e).mean()
@@ -107,23 +109,17 @@ class RmseRecorder(object):
 
 
 def plot(method_names, losses_method_a, loss_method_b, 
-         title=None, xlabel=None, ylabel=None, loglog=False, fn_out=None):
+         title='', xlabel='', ylabel='', loglog=False):
 
     import matplotlib.pyplot as plt
-
+    
     fig, ax = plt.subplots()
 
     ax.tick_params(axis='both', which='major', labelsize=12)
     ax.tick_params(axis='both', which='minor', labelsize=10)
-
-    if title:
-        ax.set_title(title, fontsize=20)
-
-    if xlabel:
-        ax.set_xlabel(xlabel, fontsize=15)
-
-    if ylabel:
-        ax.set_ylabel(ylabel, fontsize=15)
+    ax.set_title(title, fontsize=20)
+    ax.set_xlabel(xlabel, fontsize=15)
+    ax.set_ylabel(ylabel, fontsize=15)
 
     # losses with the first method
     if loglog:
@@ -138,12 +134,11 @@ def plot(method_names, losses_method_a, loss_method_b,
     ax.legend(fontsize=15)
     plt.show()
 
-    if fn_out:
-        fig.savefig(fn_out)
 
-def main(csv_fn, target_name, testing_set_ratio, lrate, epochs, normalize_features=True):
+def main(csv_fn, target_name, validation_set_ratio, lrate, epochs, 
+         norm_features=True, loglog=False):
     """
-        normalize_features: linearly rescale 
+        norm_features: linearly rescale 
     """
 
     values, names = parse_csv(csv_fn)
@@ -170,32 +165,32 @@ def main(csv_fn, target_name, testing_set_ratio, lrate, epochs, normalize_featur
     X = values[:, feature_idxs]
     y = values[:, target_idx]
 
-    # sample training and testing sets
-    test_n = int(floor(N * testing_set_ratio + .5))
-    X_test = X[:test_n]
-    y_test = y[:test_n]
-    X_train = X[test_n:]
-    y_train = y[test_n:]
+    # sample training and validation sets
+    valid_n = int(floor(N * validation_set_ratio + .5))
+    X_valid = X[:valid_n]
+    y_valid = y[:valid_n]
+    X_train = X[valid_n:]
+    y_train = y[valid_n:]
 
-    if normalize_features:
+    if norm_features:
         # we normalize based on the statistics of only the training set
         mu = np.mean(X_train, axis=0)
         std = np.std(X_train, axis=0)
 
         X_train = (X_train - mu) / std
-        X_test = (X_test - mu) / std
+        X_valid = (X_valid - mu) / std
 
-    # an object to record the loss on the testing set after every epoch
-    recorder = RmseRecorder(X=X_test, y=y_test, epochs=epochs)
+    # an object to record the loss on the validing set after every epoch
+    recorder = RmseRecorder(X=X_valid, y=y_valid, epochs=epochs)
 
     # predict with the two methods
-    ne_pred, ne_params = predict_with_linear_neuron(X_train, y_train, X_test, 
+    ne_pred, ne_params = predict_with_linear_neuron(X_train, y_train, X_valid, 
                             lrate, epochs, recorder.record)
-    lr_pred, lr_params = predict_with_linear_regression(X_train, y_train, X_test)
+    lr_pred, lr_params = predict_with_linear_regression(X_train, y_train, X_valid)
 
     method_names = ('a single linear neuron', 'linear regression baseline')
-    method_losses = (RmseRecorder.rmse(ne_pred, y_test), 
-                     RmseRecorder.rmse(lr_pred, y_test))
+    method_losses = (RmseRecorder.rmse(ne_pred, y_valid), 
+                     RmseRecorder.rmse(lr_pred, y_valid))
     method_params = (ne_params, lr_params)
 
     # print results
@@ -203,21 +198,86 @@ def main(csv_fn, target_name, testing_set_ratio, lrate, epochs, normalize_featur
         print('The loss with {0} is: {1}'.format(name, loss))
         print('The parameteres of {0} are: \n{1!s}\n'.format(name, params))
 
-    # show how the loss of a single neuron model during gradient descent 
-    # compares to that of LR
+    # show in a plot how the loss of a single neuron model during gradient 
+    # descent compares to that of LR
+    title_patt = ('RMSE when regressing "{0}" using a linear neuron model '
+                  '\ntrained with gradient descent (learning rate {1})')
+    title = title_patt.format(target_name, lrate)
+    xlabel = 'epoch' 
+    ylabel = 'RMSE (on the validation set)' 
+
     plot(method_names, recorder.losses, method_losses[1], 
-         title='RMSE for regressing "{0}" using a linear neuron model trained '
-               'with gradient descent (learning rate {1})'.
-                    format(target_name, lrate),
-         xlabel='epoch', 
-         ylabel='RMSE (on the validation set)')
+         title=title, xlabel=xlabel, ylabel=ylabel, loglog=loglog)
+
 
 if __name__ != None:
 
+    import argparse
+
     np.set_printoptions(precision=4, suppress=True)
 
-    main(csv_fn='dataset/mass_boston.csv', 
-         testing_set_ratio=0.1, 
-         target_name=None,
-         lrate=0.1, 
-         epochs=200)
+    parser = argparse.ArgumentParser(
+                description='Regression using a single linear neuron model, '
+                            'trained with gradient descent.', 
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    
+    parser.add_argument('--csv', 
+                        help='Relative path to the CSV file with the dataset', 
+                        action='store', 
+                        type=str, 
+                        metavar='FILENAME', 
+                        dest='csv_fn', 
+                        required=True)
+    
+    parser.add_argument('--target-name', 
+                        help='The name of the variable to be regressed. If none'
+                             ' is given then the last variable of the CSV is'
+                             ' used as such', 
+                        action='store', 
+                        default=None, 
+                        type=str, 
+                        metavar='NAME')
+    
+    parser.add_argument('--validation-set-ratio', 
+                        help='The fraction of the dataset to be used for '
+                              'validation', 
+                        action='store', 
+                        default=0.1, 
+                        type=float, 
+                        metavar='RATIO')
+
+    parser.add_argument('--epochs', 
+                        help='Number of epochs (iterations) to use for '
+                             'gradient descent', 
+                        action='store', 
+                        default=200, 
+                        type=int)
+
+    parser.add_argument('--lrate', 
+                        help='Learning rate to use for gradient descent', 
+                        action='store', 
+                        default=0.1, 
+                        type=float)
+
+    parser.add_argument('--no-norm', 
+                        help='By default, the features are linearly rescaled to'
+                             ' a zero mean and a standard deviation of one.'
+                             ' This switch prevents this default behavior', 
+                        action='store_true', 
+                        default=False)
+
+    parser.add_argument('--loglog', 
+                        help='use a logarithmic scale for the two axes '
+                              'of the plot', 
+                        action='store_true', 
+                        default=False)
+
+    args = parser.parse_args()
+
+    main(csv_fn=args.csv_fn, 
+         validation_set_ratio=args.validation_set_ratio, 
+         target_name=args.target_name,
+         lrate=args.lrate, 
+         epochs=args.epochs, 
+         norm_features=not(args.no_norm), 
+         loglog=args.loglog)
